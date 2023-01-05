08be5601af5381659bc736fa2f33ccb1a83426b3
mina <m1nna@naver.com>
2023-01-05 12:58:56 +0900

    service insertBook() test - mockito 활용
    stub, mock, assertJ

--- src/test/java/com/example/junit/service/BookServiceTest.java
+++ src/test/java/com/example/junit/service/BookServiceTest.java
@@ -1,21 +1,32 @@
 package com.example.junit.service;
 
 import com.example.junit.domain.BookRepository;
-import com.example.junit.util.MailSenderStub;
+import com.example.junit.util.MailSender;
 import com.example.junit.web.dto.BookRespDto;
 import com.example.junit.web.dto.BookSaveReqDto;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@DataJpaTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
-    @Autowired
-    private BookRepository bookRepository;
 
-    // 현재 상황: 서비스와 레포지토리 레이어가 함께 테스트 되는 상황
+    @InjectMocks
+    private BookService bookService;
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private MailSender mailSender;
+
     @Test
     public void 책등록하기_테스트(){
         // given
@@ -24,14 +35,16 @@ public class BookServiceTest {
         dto.setAuthor("metaCoding");
 
         // stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
+        when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
 
         // when
-         BookService bookService = new BookService(bookRepository, mailSenderStub);
          BookRespDto bookRespDto = bookService.insertBook(dto);
 
         // then
-        assertEquals(dto.getTitle(), bookRespDto.getTitle());
-        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+//        assertEquals(dto.getTitle(), bookRespDto.getTitle());
+//        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
     }
 }
